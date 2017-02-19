db51525d06244b52b425f1b04fe7869dff1f2bd7
Yogesh Linganna <yogesh.linganna@siemens.com>
2017-02-19 16:56:29 +0530

    Included Babel transpiler

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+//var express = require('express');
+import express from 'express';
+//var path = require('path');
+import path from 'path';
+//var open = require('open');
+import open from 'open';
 
-var port = 3000;
-var app = express();
+//var port = 3000;
+const port = 3000;
+//var app = express();
+const app = express();
 
 app.get('/',function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,6 @@
-var chalk = require('chalk');
+//var chalk = require('chalk');
+
+//ES6 systax - Babel will transpile
+import chalk from 'chalk';
 
 console.log(chalk.green('Starting app in dev mode...'));
